c543cc8197cd8270982f5a3540707e01dcdd5d04
Andy Wickert <andy@northernwidget.com>
2016-10-05 10:02:03 -0500

    Skip non-numeric inputs; KML output added

--- drainage_basin_histories_2016/ExportBasins.py
+++ drainage_basin_histories_2016/ExportBasins.py
@@ -8,7 +8,9 @@ def get_time_steps():
   indexmaps = sorted(indexmaps)[::-1]
   ages = []
   for indexmap in indexmaps:
-    ages.append( str(indexmap.split('_')[-1]) )
+    age = str(indexmap.split('_')[-1])
+    if len(age) == 6:
+      ages.append( age )
   return ages
   
 # Limit plotted rivers to those that are being shown in the plots
@@ -18,8 +20,19 @@ wherestr = "river = 'Mississippi' OR river = 'Colorado' OR river = 'Rio Grande' 
 # EXPORT BASINS AT ALL TIME-STEPS
 ages = get_time_steps()
 for age in ages:
+  print age
   drainage_basins = 'drainage_basins_'+age
   drainage_basins_ESurf = 'drainage_basins_ESurf_'+age
-  grass.run_command('v.extract', input=drainage_basins, output='drainage_basins_ESurf_', where=wherestr, overwrite=True)
-  grass.run_command('v.out.ogr', input='drainage_basins_ESurf_', output=drainage_basins)
+  #grass.run_command('v.build', map=drainage_basins, quiet=True)
+  grass.run_command('v.extract', input=drainage_basins, output='drainage_basins_ESurf_', where=wherestr, overwrite=True, quiet=True)
+  try:
+    grass.run_command('v.out.ogr', input='drainage_basins_ESurf_', output=drainage_basins, quiet=True)
+  except:
+    print "... > Shapefile exists!"
+
+for age in ages:
+  try:
+    grass.run_command('v.out.ogr', input='drainage_basins_ESurf_', output=drainage_basins+'.kml', format='KML', quiet=True)
+  except:
+    print "... > KML exists!"
 
